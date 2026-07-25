257e79506a4fab08031d28308a84b58738382bc2
Nelson <molina9420@gmail.com>
2026-07-25 02:56:24 -0400

    fix: modal base scrollable so settings dont get cut off

--- src/modals/ModalBase/styles.ts
+++ src/modals/ModalBase/styles.ts
@@ -3,7 +3,7 @@ import { styled } from '@linaria/react';
 export const Container = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
 
   position: fixed;
   top: 0;
@@ -11,6 +11,9 @@ export const Container = styled.div`
   right: 0;
   bottom: 0;
   z-index: 9999;
+
+  overflow-y: auto;
+  padding: 2rem 0;
 
   background: rgba(0, 0, 0, 0.65);
   backdrop-filter: blur(6px);
